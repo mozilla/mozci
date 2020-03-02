4275ddeb0bd61fa3acc94a3ed726defced5d04dd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-02 12:53:03 +0100

    Add some simil-integration tests to ensure manifest data is correct

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,6 +3,8 @@ from argparse import Namespace
 
 from adr.query import run_query
 
+from mozci import task
+
 
 def test_missing_manifests():
     """
@@ -56,3 +58,37 @@ def test_missing_result_manifests():
     }
     for suite in BLACKLIST:
         assert suite in found_suites, f"{suite} might be removed from the blacklist"
+
+
+def test_good_manifests():
+    """
+    Ensure there are no bad manifest paths in recent manifest information.
+    """
+    result = run_query("test_all_groups", Namespace())
+
+    for (groups,) in result["data"]:
+        if groups is None:
+            continue
+
+        if not isinstance(groups, list):
+            groups = [groups]
+
+        for group in groups:
+            assert (
+                not task.is_bad_group("x", group) and "\\" not in group
+            ), f"{group} group is bad!"
+
+
+def test_good_result_manifests():
+    """
+    Ensure there are no bad manifest paths in recent result manifest information.
+    """
+    result = run_query("test_all_result_groups", Namespace())
+
+    for group, count in result["data"]:
+        if group is None:
+            continue
+
+        assert (
+            not task.is_bad_group("x", group) and "\\" not in group
+        ), f"{group} group is bad!"

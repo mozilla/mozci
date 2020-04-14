aa2790a592cfa5d4bdef459c1feded2583c26d4a
Kyle Lahnakoski <klahnakoski@mozilla.com>
2020-04-14 06:50:11 -0400

    Use default values in the util/req module (#149)
    So users can change them.
    
    Fixes #147

--- mozci/util/req.py
+++ mozci/util/req.py
@@ -4,12 +4,21 @@ from functools import lru_cache
 import requests
 from requests.packages.urllib3.util.retry import Retry
 
+# https://urllib3.readthedocs.io/en/latest/reference/urllib3.util.html#module-urllib3.util.retry
+DEFAULT_RETRIES = 5
+DEFAULT_BACKOFF_FACTOR = 0.1
+DEFAULT_STATUS_FORCELIST = [500, 502, 503, 504]
+
 
 @lru_cache(maxsize=None)
 def get_session(name, concurrency=50):
     session = requests.Session()
 
-    retry = Retry(total=5, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
+    retry = Retry(
+        total=DEFAULT_RETRIES,
+        backoff_factor=DEFAULT_BACKOFF_FACTOR,
+        status_forcelist=DEFAULT_STATUS_FORCELIST,
+    )
 
     # Default HTTPAdapter uses 10 connections. Mount custom adapter to increase
     # that limit. Connections are established as needed, so using a large value

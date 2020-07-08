mozilla/mozci

commit ab5fe1325ad5045d738c698e276b6283dc037f4b

Make sure to always remove _result_group when normalizing task objects (#235)
Since 846798e5b9db6e3a33dd58ba0b14d19b8585f5a1, there might be cases where we have _result_group and no _result_ok
(since the ok_intermittent value is not available yet in all unittest results).

mozci/push.py
@@ -367,11 +367,11 @@ class Push:
                 if isinstance(task["classification_note"], list):
                     task["classification_note"] = task["classification_note"][-1]
 
-            if task.get("_result_ok"):
-                oks = task.pop("_result_ok")
+            if task.get("_result_group"):
+                groups = task.pop("_result_group")
 
-                if task.get("_result_group"):
-                    groups = task.pop("_result_group")
+                if task.get("_result_ok"):
+                    oks = task.pop("_result_ok")
 
                     task["_results"] = [
                         GroupResult(group=group, ok=ok)

commit 5f324025ce0985966ecdec5c58be03d1f33763cb

Make sure to always remove _result_ok when normalizing task objects
There are be cases where we have _result_ok and no _result_group, e.g. Talos

Fixes a regression from ab5fe1325ad5045d738c698e276b6283dc037f4b

mozci/push.py
@@ -368,12 +368,11 @@ class Push:
                 if isinstance(task["classification_note"], list):
                     task["classification_note"] = task["classification_note"][-1]
 
-            if task.get("_result_group"):
-                groups = task.pop("_result_group")
+            groups = task.pop("_result_group", None)
+            oks = task.pop("_result_ok", None)
 
-                if task.get("_result_ok"):
-                    oks = task.pop("_result_ok")
-
+            if groups is not None:
+                if oks:
                     task["_results"] = [
                         GroupResult(group=group, ok=ok)
                         for group, ok in zip(groups, oks)
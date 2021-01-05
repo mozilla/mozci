2c6a5404c6ac9260ea256aebf25233447af927f3
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-01-05 16:47:35 +0100

    Stop using the adr data source by default

--- mozci/configuration.py
+++ mozci/configuration.py
@@ -102,7 +102,7 @@ class Configuration(Mapping):
             "cache": {"retention": 1440},
         },  # minutes
         "replace": {
-            "data_sources": ["adr"],
+            "data_sources": ["hgmo", "taskcluster", "treeherder_client"],
             "verbose": False,
         },
     }

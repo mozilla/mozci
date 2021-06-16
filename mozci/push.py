mozilla/mozci

commit e488ccd4966163ed9532ab51f4584fc60286ae10

Only try to gather groups from completed tasks
This also fixes a bug in the taskcluster data source where
we normalize 'exception' to 'completed'.

In treeherder exception means the task ran and had a problem. But in
taskcluster land, an exception mean the task never even ran. E.g, a task
can be in this state if its dependency failed and then hit an expiry.

mozci/push.py
@@ -285,6 +285,7 @@ class Push:
                 )
                 for task in tasks
                 if isinstance(task, TestTask)
+                if task.state == "completed"
             ],
             return_when=concurrent.futures.FIRST_EXCEPTION,
         )

mozci/data/contract.py
@@ -33,7 +33,13 @@ _contracts: Tuple[Contract, ...] = (
                     "id": v.Str(),
                     "label": v.Str(),
                     "state": v.Str(
-                        options=["completed", "running", "pending", "unscheduled"]
+                        options=[
+                            "completed",
+                            "running",
+                            "pending",
+                            "unscheduled",
+                            "exception",
+                        ]
                     ),
                     "tags": v.Dict(extra=(v.Str(), v.Str())),
                     "duration": v.Int(),

mozci/data/sources/taskcluster/__init__.py
@@ -64,7 +64,7 @@ class TaskclusterSource(DataSource):
                 run = result["status"]["runs"][-1]
 
                 # Normalize the state to match treeherder's values.
-                if task["state"] in ("failed", "exception"):
+                if task["state"] == "failed":
                     task["state"] = "completed"
 
                 # Derive a result from the reasonResolved.
@@ -77,7 +77,7 @@ class TaskclusterSource(DataSource):
                     task["result"] = "exception"
                 else:
                     # Task is not finished, so there is no result yet.
-                    assert task["state"] in ("pending", "running")
+                    assert task["state"] in ("pending", "running", "exception")
 
                 # Compute duration.
                 if "started" in run and "resolved" in run:

tests/test_data_sources.py
@@ -104,6 +104,20 @@ class Responses:
                         "task": {
                             "extra": {},
                             "metadata": {
+                                "name": "task-B-2",
+                            },
+                            "tags": {},
+                        },
+                        "status": {
+                            "taskId": "task-id-B-2",
+                            "state": "exception",
+                            "runs": [{}],
+                        },
+                    },
+                    {
+                        "task": {
+                            "extra": {},
+                            "metadata": {
                                 "name": "task-C",
                             },
                             "tags": {"name": "tag-C"},
@@ -283,6 +297,12 @@ class Responses:
                     "tags": {"name": "tag-A"},
                 },
                 {"id": "task-id-B", "label": "task-B", "state": "pending", "tags": {}},
+                {
+                    "id": "task-id-B-2",
+                    "label": "task-B-2",
+                    "state": "exception",
+                    "tags": {},
+                },
                 {
                     "id": "task-id-C",
                     "label": "task-C",

commit ae94ab435a438e5c22d5abb273397dadd182806d

Set incomplete task results to an empty list
The task.results attribute expects the _results property to have a value.
Since e488ccd4966163ed9532ab51f4584fc60286ae10, we don't populate _results
at the time of task creation for incomplete tasks (e.g. tasks ending with
'exception'). Later on, we try to access the task.results attribute for all
tasks, even incomplete ones, and so the assertion on _results not being None
fails.

Fixes #473, a regression from e488ccd4966163ed9532ab51f4584fc60286ae10

mozci/push.py
@@ -285,7 +285,6 @@ class Push:
                 )
                 for task in tasks
                 if isinstance(task, TestTask)
-                if task.state == "completed"
             ],
             return_when=concurrent.futures.FIRST_EXCEPTION,
         )

mozci/task.py
@@ -251,12 +251,15 @@ class TestTask(Task):
 
             return
 
-        self._results = [
-            GroupResult(group, result)
-            for group, result in data.handler.get(
-                "test_task_groups", branch=push.branch, rev=push.rev, task=self
-            ).items()
-        ]
+        if self.state == "completed":
+            self._results = [
+                GroupResult(group, result)
+                for group, result in data.handler.get(
+                    "test_task_groups", branch=push.branch, rev=push.rev, task=self
+                ).items()
+            ]
+        else:
+            self._results = []
 
         # Apply WPT workaround, needed at least until bug 1632546 is fixed.
         if self.is_wpt:
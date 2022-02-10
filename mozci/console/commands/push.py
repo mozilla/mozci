f929ff8dfeedabf6fd98a5d0276429e272ab3243
Marco Castelluccio <mcastelluccio@mozilla.com>
2022-02-10 23:08:34 +0100

    Pretend there were no manual classifications when evaluating the autoclassification results
    Fixes #606

--- mozci/console/commands/push.py
+++ mozci/console/commands/push.py
@@ -280,6 +280,11 @@ class ClassifyEvalCommand(Command):
         self.errors = {}
         self.classifications = {}
         for push in self.pushes:
+            # Pretend no tasks were classified to evaluate the model without any outside help.
+            for task in push.tasks:
+                task.classification = "not classified"
+                task.classification_note = None
+
             if self.option("recalculate"):
                 progress.set_message(f"Calc. {branch} {push.id}")
                 try:

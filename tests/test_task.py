d6cecc9e27d359e01e38d5716f9019cc510f78bb
Marco Castelluccio <mcastelluccio@mozilla.com>
2022-07-19 11:22:11 +0200

    Allow autoclassification when there are multiple failures of the same type reported for the same test (#817)
    * Allow autoclassification when there are multiple failures of the same type reported for the same test
    
    * Add a test with a test failing in the same group in two different modes

--- tests/test_task.py
+++ tests/test_task.py
@@ -864,6 +864,12 @@ def test_autoclassify_errors(autoclassification_config, expected_error, error_me
 ONE_FAILURE_TYPE_CRASH = {"group1": [("test1.js", FailureType.CRASH)]}
 ONE_FAILURE_TYPE_TIMEOUT = {"group1": [("test1.js", FailureType.TIMEOUT)]}
 ONE_FAILURE_TYPE_GENERIC = {"group1": [("test1.js", FailureType.GENERIC)]}
+TWO_FAILURES_SAME_TEST_TYPE_GENERIC = {
+    "group1": [("test1.js", FailureType.GENERIC), ("test1.js", FailureType.GENERIC)]
+}
+TWO_FAILURES_SAME_TEST_DIFFERENT_TYPE = {
+    "group1": [("test1.js", FailureType.GENERIC), ("test1.js", FailureType.CRASH)]
+}
 MULTIPLE_FAILURE_TYPES = {
     "group1": [("test1.js", FailureType.CRASH), ("test2.js", FailureType.CRASH)],
     "group2": [("test1.js", FailureType.GENERIC), ("test2.js", FailureType.TIMEOUT)],
@@ -937,6 +943,18 @@ MULTIPLE_FAILURE_TYPES = {
         (ONE_FAILURE_TYPE_GENERIC, True, ["*"], ["crash", "timeout"], False),
         ({}, True, ["*"], ["crash"], False),
         (MULTIPLE_FAILURE_TYPES, True, ["*"], ["crash"], False),
+        # Two generic failures in the same test.
+        (TWO_FAILURES_SAME_TEST_TYPE_GENERIC, True, ["*"], ["generic"], True),
+        # A generic failure and a crash in the same test.
+        (TWO_FAILURES_SAME_TEST_DIFFERENT_TYPE, True, ["*"], ["generic"], False),
+        (TWO_FAILURES_SAME_TEST_DIFFERENT_TYPE, True, ["*"], ["crash"], False),
+        (
+            TWO_FAILURES_SAME_TEST_DIFFERENT_TYPE,
+            True,
+            ["*"],
+            ["generic", "crash"],
+            False,
+        ),
     ],
 )
 def test_autoclassify(

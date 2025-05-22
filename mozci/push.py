9c32efd45d157a74d2b5ecdec0933a5109204da4
Valentin Rigal <rigal@teklia.com>
2025-05-22 17:31:58 +0200

    Improve logging for build regression analysis (#1162)
    * Fix erroneous logging message about errors with no previous occurrence
    
    * Improve logging a bit

--- mozci/push.py
+++ mozci/push.py
@@ -1139,21 +1139,24 @@ class Push:
             and task.is_build_failure()
         ]
         if not build_regressions:
-            logger.info("No regression detected.")
+            logger.info("No build regression detected.")
             return []
 
-        new_regressions = sum(occurrences > 0 for _, occurrences in build_regressions)
+        new_regressions = sum(past_occurrences == 0 for _, past_occurrences in build_regressions)
         logger.info(
             f"Detected {len(build_regressions)} build tasks that may contain a regression "
-            f"({new_regressions} potentially introduced by this push)."
+            f"({new_regressions} were never seen before)."
         )
 
-        # Filter tasks by retrigger criteria
-        return [
+        tasks_to_retrigger = [
             task
             for task, count in build_regressions
             if task.should_retrigger_build(previous_occurrences_count=count)
         ]
+        if not tasks_to_retrigger:
+            logger.info("No build task should be retriggered.")
+        logger.info("{len(tasks)} build tasks should be retriggered.")
+        return tasks_to_retrigger
 
     def classify_regressions(
         self,

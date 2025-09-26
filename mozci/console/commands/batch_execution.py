f64b7bc1e94a5cf56ecac943c2888cfb588b5adf
Valentin Rigal <rigal@teklia.com>
2025-09-26 17:46:46 +0200

    Add entrypoint in push classification

--- mozci/console/commands/batch_execution.py
+++ mozci/console/commands/batch_execution.py
@@ -92,6 +92,8 @@ def run_combinations_for_push(push):
         start = time.time()
         try:
             classification, regressions = push.classify(**parameters)
+            # Rettriger test failures to detect permanent ones, only on Linux cloud worker pool for now
+            push.identify__permanent_failures(queue_prefix="gecko-t/t-linux-")
             end = time.time()
 
             # Only save results to a JSON file if the execution was successful

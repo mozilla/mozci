0626df9786ed032ecf1323ca3a4dd1e932ac29fa
Joel Maher <joel.maher@gmail.com>
2025-10-20 08:07:33 -0700

    Add new classification id for LPID (#1204)

--- mozci/util/defs.py
+++ mozci/util/defs.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
 FAILURE_CLASSES = ("not classified", "new failure not classified", "fixed by commit")
-INTERMITTENT_CLASSES = ("intermittent", "autoclassified intermittent")
+INTERMITTENT_CLASSES = ("intermittent", "autoclassified intermittent", "intermittent needs bugid")
 TASK_FINAL_STATES = ("completed", "failed", "exception")
